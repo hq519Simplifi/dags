a72f9c24a7d99d5889e29b1e3394eca1198bc575
hqGitAcc <quhai519@gmail.com>
2023-01-18 18:01:19 -0600

    time update

--- testdag.py
+++ testdag.py
@@ -16,7 +16,7 @@ local_tz = pendulum.timezone("America/Chicago")
 default_args = {
     "owner": "airflow",
     "depends_on_past": True,
-    'start_date': datetime(year=2023, month=01, day=17, hour=0, tzinfo=local_tz),
+    'start_date': datetime(year=2023, month=1, day=17, hour=0, tzinfo=local_tz),
     "email": ["quhai519@gmail.com"],
     "email_on_failure": True,
     "email_on_retry": True,
