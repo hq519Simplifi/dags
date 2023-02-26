cc2a0b4cbff8ed68abb23d2a0d3be19cce9e7613
hqGitAcc <quhai519@gmail.com>
2023-02-25 22:22:37 -0600

    dd

--- testdag.py
+++ testdag.py
@@ -29,7 +29,7 @@ default_args = {
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG("testAll", default_args=default_args, schedule_interval=timedelta(1))
+dag = DAG("testAll", default_args=default_args, schedule_interval=timedelta(1), catchup=False)
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = SqlSensor(
